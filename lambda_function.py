1c5a26835a51db90f1c53897c00b6165c1db7df7
abdurrehman11 <abdurrehman245@gmail.com>
2024-03-14 22:11:21 +0500

    sns topic added

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,9 @@
-import json
+import boto3
 import requests
 import pandas as pd
+
+sns_client = boto3.client('sns')
+sns_arn = "arn:aws:sns:us-east-1:471112663332:cicd-trigger-notification"
 
 def lambda_handler(event, context):
     
@@ -10,6 +13,14 @@ def lambda_handler(event, context):
 
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
-    
+
+    message = "Lambda function triggered via CICD"
+    response = sns_client.publish(
+        Subject="SUCCESS- Lambda CICD pipeline", 
+        TargetArn=sns_arn, 
+        Message=message, 
+        MessageStructure='text'
+    )
+
     print(df)
     print('Demo Completed !!!')
